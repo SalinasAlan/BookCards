f51f465c241cccd5c9e168578818560048fcfad5
SalinasAlan <alansal_11@hotmail.com>
2019-08-27 23:42:52 -0500

    Se generó código

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const express = require('express');
 const morgan = require('morgan');
 const multer = require('multer');
 const path = require('path');
+const cors = require('cors');
 
 
 // Inicializations
@@ -20,12 +21,13 @@ app.use(morgan('dev'));
 const storage = multer.diskStorage({
     destination: path.join(__dirname, 'public/uploads'),
     filename(req, file, cb){
-        cd(null, new Date().getTime() + path.extname(file.originalname));
+        cb(null, new Date().getTime() + path.extname(file.originalname));
     }
 });
-app.use(multer(storage).single('image'));
+app.use(multer({storage}).single('image'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use('/api/books', require('./routes/books'));
