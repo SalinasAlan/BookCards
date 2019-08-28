f1a3fb0f9a956d872f1a6ea99f1fee3a544e0bb5
SalinasAlan <alansal_11@hotmail.com>
2019-08-27 23:41:53 -0500

    Se agregó las funciones para elminar y agregar imagenes

--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,5 +1,7 @@
 const {Router} = require('express');
 const router = Router();
+const {unlink} = require('fs-extra');
+const path = require('path')
 
 const book = require('../models/Book');
 
@@ -10,13 +12,15 @@ router.get('/', async (req, res) => {
 
 router.post('/', async (req, res) => {
     const {title, author, isbn} = req.body;
-    const newBook = new book({title, author, isbn});
+    const imagePath = '/uploads/' + req.file.filename;
+    const newBook = new book({title, author, isbn, imagePath});
     await newBook.save();
     res.json({message: 'Book saved'});    
 });
 
 router.delete('/:id', async (req, res) => {
-    await book.findByIdAndDelete(req.params.id);
+    const bookDelete = await book.findByIdAndDelete(req.params.id);
+    unlink(path.resolve('./backend/public/' + bookDelete.imagePath));
     res.json({message: 'Book deleted'});    
 });
 
